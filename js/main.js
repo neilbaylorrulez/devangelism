f4f71ff04fc8d80c51b872a1d6ab72f9190baf96
neilbaylorrulez <neil.t@myplanetdigital.com>
2014-07-22 12:09:22 -0400

    front end improvements

--- js/main.js
+++ js/main.js
@@ -1,47 +1,59 @@
 (function () {
 	'use strict';
 
-	var $body = $('body'),
+	var $toHideOverflow = $('html, body'),
+		$body = $('body'),
 		$wrap = $('#wrap'),
 		$nextPage,
 		scrollFn,
+		internalHashChange = false,
 		SCROLL_TRANSITION_TIME = 800;
 
 	function easeInOut(t, b, c, d) {
 		if ((t/=d/2) < 1) {
 			return c/2*t*t + b;
-        }
-        return -c/2 * ((--t)*(t-2) - 1) + b;
+		}
+
+		return -c/2 * ((--t)*(t-2) - 1) + b;
+	}
+
+	function finishScrollTo(y, newY, newUrl, afterFn) {
+		window.scrollTo(0, newY);
+		window.location.hash = newUrl === 'home' ? '' : newUrl;
+
+		if(afterFn) {
+			afterFn();
+		}
 	}
 
 	function skrollTo(startTime, y, newY, newUrl, afterFn) {
 		var now = Date.now(),
 			duration = now - startTime,
 			val = easeInOut(duration, y, newY, SCROLL_TRANSITION_TIME);
-			console.log(val);
 		window.scrollTo(0, val);
 
 		if(y !== newY && duration < SCROLL_TRANSITION_TIME) {
-			window.requestAnimationFrame(scrollFn);
-		} else {
-			window.scrollTo(0, y);
-			if(afterFn) {
-				afterFn();
-			}
-			window.location.hash = newUrl;
+			return window.requestAnimationFrame(scrollFn);
 		}
+		finishScrollTo(y, newY, newUrl, afterFn);
 	}
 
-	function skrollToPage($page, afterFn) {
+	function scrollToPage($page, afterFn) {
 		var top = window.pageYOffset,
-			newTop = $page.offset().top;
-		window.setTimeout(window.requestAnimationFrame(scrollFn = skrollTo.bind(null, Date.now(), top, newTop - top, $page.attr('id'), afterFn)), 0);
+			newTop = $page.offset().top,
+			newUrl = $page.attr('id');
+		console.log(top + ' ' + newTop);
+		if(top === newTop) {
+			return finishScrollTo(top, newTop, newUrl, afterFn);
+		}
+
+		window.setTimeout(window.requestAnimationFrame.bind(null, scrollFn = skrollTo.bind(null, Date.now(), top, newTop - top, newUrl, afterFn)), 0);
 	}
 
 	function showContactPage() {
-		skrollToPage($('#contact'), function () {
+		scrollToPage($('#contact'), function () {
 			$wrap.addClass('contact');
-			document.body.style.overflow = 'hidden';
+			$toHideOverflow.css('overflow', 'hidden');
 		});
 	}
 
@@ -51,32 +63,42 @@
 			return showContactPage();
 		}
 
-		skrollToPage($page);
+		scrollToPage($page);
 	}
 
 	function initState() {
+		$wrap.addClass('loaded');
 		if(window.location.hash === '#contact') {
 			showContactPage();
 		}
-
-		window.setTimeout(function () {
-			$wrap.addClass('loaded');
-		}, 0);
 	}
 
 	function initListeners() {
 		$body.on('click', 'a', function(e) {
-			var $page = $(e.currentTarget.getAttribute('href'));
+			var curHash = window.location.hash,
+				anchor = e.currentTarget.getAttribute('href'),
+				$page = $(anchor);
 			if($page.length) {
+				internalHashChange = curHash !== anchor;
 				showPage($page);
 				return false;
 			}
 		});
 
-		$body.on('transitionEnd webkitTransitionEnd', function (e) {
+		$body.on('transitionend webkitTransitionEnd', function (e) {
 			if(e.target.id === 'contact' && $nextPage && $nextPage.length && $nextPage.attr('id') === 'contact') {
 				window.location.hash = 'contact';
 			}
+		});
+
+		$(window).on('hashchange', function() {
+			if(!internalHashChange) {
+				var $page = $(window.location.hash || '#home');
+				if($page.length) {
+					showPage($page);
+				}
+			}
+			internalHashChange = false;
 		});
 	}
 
