12f747ccefdd702666ab03e21bc2099f4c0f2838
neilbaylorrulez <neil.t@myplanetdigital.com>
2014-08-20 12:33:03 -0400

    new transition

--- js/pages/home.js
+++ js/pages/home.js
@@ -5,6 +5,7 @@
 
 	function animate() {
 		$weLoveDevs.html($weLoveDevs.text().replace(/([^\x00-\x80]|\w)/g, "<span>$&</span>"));
+		$weLoveDevs.find('span').css('transform', 'translate3d(' + window.viewportWidth + 'px,0,0) scale(1)');
 		window.setTimeout(window.requestAnimationFrame.bind(null, function () {
 			$weLoveDevs.addClass('animate');
 			window.setTimeout(function () {
